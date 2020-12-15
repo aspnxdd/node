e11085f485a2dd60b9f2317f5992d6c400cd3b27
Arnau Espín <arnauespin@gmail.com>
2020-12-15 22:24:09 +0100

    Canvi 2

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+// import { FirebaseContext } from './contexts/firebase';
+// import { Firebase, auth, analytics } from './lib/firebase';
+
 const express = require("express");
 let server = express()
+const jwt = require("jsonwebtoken")
 
 const { connectar: connect } = require('./database.js')
 const dbName = 'test';
@@ -7,17 +11,14 @@ const dbName = 'test';
 server.engine('htmls', require('ejs').renderFile);
 server.set('view engine', 'htmls');
 
+server.use('/', express.static('./static'));
+
+const superPass = "wow"
+
 server.get('/', function (req, res) {
+    console.log(req)
     const db = client.db(dbName)
     const collection = db.collection("materials");
-    // collection.find({}).toArray(function (err, materials_list) {
-    //     assert.equal(err, null);
-    //     console.log("collection loaded");
-    //     console.log(materials_list);
-    //     res.render("index2.ejs", {
-    //         materials: materials_list
-    //     });
-    // });
     collection.aggregate([
         {
             $lookup:
@@ -36,7 +37,30 @@ server.get('/', function (req, res) {
         });
     });
 });
-//
-//npm run dev
+
+function getUserToken(usuari) {
+    return jwt.sign(usuari, superPass)
+}
+
+server.post('/login', function (req, res) {
+    console.log(req.headers)
+    const { nom, pass } = req.headers
+
+    const db = client.db(dbName)
+    const collection = db.collection("auth")
+
+    collection.findOne({
+        nom,
+        pass
+    }, (err, usuari) => {
+        console.log("usuari", err, usuari, nom, pass)
+        if (usuari.pass == pass) {
+            res.json({
+                token: getUserToken(usuari)
+            })
+        }
+    })
+})
+
 const client = connect(server);
 
